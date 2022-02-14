a680942c0a844a4f7bf3c68da378e792845eee3a
nairdesigns <nairbdg@gmail.com>
2022-02-14 16:50:05 +0900

    api is working

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,23 @@
+require 'uri'
+require 'net/http'
+
 class GamesController < ApplicationController
   def new
     @letters = (0..10).map { ('a'..'z').to_a[rand(26)] }.join('  ')
   end
 
   def score
-    raise
+    url = URI('https://wagon-dictionary.herokuapp.com/hey')
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(url)
+    request['x-api-key'] = ENV['SYGIC_API_KEY']
+    request['cache-control'] = 'no-cache'
+
+    @response = http.request(request)
+    @hash = JSON.parse @response.body.gsub('=>', ':')
   end
 end
